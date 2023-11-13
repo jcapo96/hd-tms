1846181ba474da3c7b896f8e355034c5256c9dea
jcapo96 <jcapo.ific@gmail.com>
2023-11-13 12:56:37 +0100

    make calibration script

--- makeCalibration.py
+++ makeCalibration.py
@@ -1,6 +1,9 @@
 from ROOTconverter.calibrationMethods import RefMethod, TreeMethod
 import pandas as pd
-import json
+import json, os
+
+pathToData = os.path.join(os.pardir, os.pardir, os.pardir, "RTDdata")
+pathToSaveData = f"{pathToData}/ProcessedData/TGrad/"
 
 file = open("keys/calibrationRunsInfo.json")
 calibration_runs = json.load(file)
@@ -21,15 +24,15 @@ LN2Tgrad_2023 = TreeMethod(
 
 LArresults = pd.DataFrame.from_dict(LArTgrad.get_calresults(bestpathBool=True)).T
 LArresults.columns = ["CC", "CC_err"]
-LArresults.to_csv("LarTree.csv", index=True)
+LArresults.to_csv(f"{pathToSaveData}LArTree.csv", index=True)
 
 
-# LN22_results = pd.DataFrame.from_dict(LN2Tgrad_2022.get_calresults(bestpathBool=True)).T
-# LN22_results.columns = ["CC", "CC_err"]
-# LN22_results.to_csv("/Users/jcapo/Documents/Software/TMS/FD1_TMS/paper/data/LN22Tree.csv", index=True)
+LN22_results = pd.DataFrame.from_dict(LN2Tgrad_2022.get_calresults(bestpathBool=True)).T
+LN22_results.columns = ["CC", "CC_err"]
+LN22_results.to_csv(f"{pathToSaveData}LN22Tree.csv", index=True)
 
 
-# LN23_results = pd.DataFrame.from_dict(LN2Tgrad_2023.get_calresults(bestpathBool=True)).T
-# LN23_results.columns = ["CC", "CC_err"]
-# LN23_results.to_csv("/Users/jcapo/Documents/Software/TMS/FD1_TMS/paper/data/LN23Tree.csv", index=True)
+LN23_results = pd.DataFrame.from_dict(LN2Tgrad_2023.get_calresults(bestpathBool=True)).T
+LN23_results.columns = ["CC", "CC_err"]
+LN23_results.to_csv(f"{pathToSaveData}LN23Tree.csv", index=True)
 
